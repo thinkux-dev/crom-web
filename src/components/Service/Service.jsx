ffc59db81fa193decce6e4359550f66c5ecbc2e8
thelion24 <bobdaawid@gmail.com>
2025-04-06 15:33:19 +0100

    business section responsiveness

---
+++ src/components/Service/Service.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Service.css';
+import hero_logo from "../../asset/img/hero_logo.png";
+import ai_automation from "../../asset/img/ai_automation_icon.png";
+import branding_img from "../../asset/img/branding_mg.jpg";
+import design_bg_img from "../../asset/img/design_bg_img.jpg";
+import design_icon_btn from "../../asset/img/design_branding_icon.png";
+import guy_with_headphone from "../../asset/img/guy_with_headphone.jpg";
+
+const Service = () => {
+  // return (
+
+  // );
+}
+
+export default Service;
--- src/pages/Website.jsx
+++ src/pages/Website.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Hero from '../components/Hero/Hero'
 import Business from '../components/Business/Business'
+import Service from '../components/Service/Service'
 
 const Website = () => {
   return (
@@ -8,6 +9,7 @@ const Website = () => {
         {/* <FeedbackButton/> */}
         <Hero/>
         <Business/>
+        <Service/>
     </div>
   )
 }
